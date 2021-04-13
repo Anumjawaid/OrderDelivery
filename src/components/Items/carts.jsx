095d1c9a65d4447f74b04621b90d64cf477a0fee
Anumjawaid <anumjawaid99@gmail.com>
2021-04-13 23:55:00 +0500

    remove cart element

--- src/components/Items/carts.jsx
+++ src/components/Items/carts.jsx
@@ -1,4 +1,6 @@
 import React,{useState} from 'react'
+import './style.css';
+
 
 export default function Cart({cart,setCart}){
 
@@ -16,11 +18,12 @@ export default function Cart({cart,setCart}){
     }
     const remove = ()=>{
         console.log("i am working")
+
     }
 
     return(
         <div>
-            <div className="container"><h4>Items In Cart</h4>
+            <div className="container main"><h4>Items In Cart</h4>
             <h3>Your Cart Items</h3>
             <button>Delete Cart</button> 
             <button>Place Your Order</button>
@@ -33,7 +36,7 @@ export default function Cart({cart,setCart}){
                             {/* <img src={b[i] }alt="" className="card-img-top"/> */}
                             <div className="card-body"><h5 className="card-title">{v['name']}</h5>
                             <p className='card-text'> Price:{v['price']} Quantity {v['quantity']}</p></div>
-                            <button onClick={remove}>Remove Item</button>
+                            <button key={i} onClick={()=>remove(i)}>Remove Item</button>
                         </div>
                     </div>
                 </div>
