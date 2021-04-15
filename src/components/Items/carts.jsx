67d71001b714ac32f4ecc49b7e8700422375952b
Anumjawaid <anumjawaid99@gmail.com>
2021-04-15 15:35:41 +0500

    remove cart done

--- src/components/Items/carts.jsx
+++ src/components/Items/carts.jsx
@@ -3,12 +3,15 @@ import './style.css';
 
 
 export default function Cart({cart,setCart}){
+    let [ncart,setNcart]=useState([])
+    ncart=cart
 
     let [page,setPage]=useState()
     // console.log(cart[0]['name'][0],"from cart")
-    console.log("cart",cart)
-    cart.map((v,i)=>(
+    console.log("cart",cart,ncart)
+    ncart.map((v,i)=>(
         console.log(v,"asdfgh")
+
        ))
     
    
@@ -16,18 +19,25 @@ export default function Cart({cart,setCart}){
         setPage(nextpage)
 
     }
-    const remove = ()=>{
-        console.log("i am working")
-
+    const remove = (i)=>{
+        console.log("i am working",i)
+        // delete cart[i]
+        ncart.splice(i,1)
+        console.log(ncart)
+        // setTimeout(() => {  setCart(cart) }, 2000);
+        setNcart([...ncart])
+        // setNcart(Object.assign({}, ncart)) this can be used as well as setNcart({...ncart})
+// 
     }
+    
 
     return(
         <div>
             <div className="container main"><h4>Items In Cart</h4>
-            <h3>Your Cart Items</h3>
+            <h3>Your Cart Ittems</h3>
             <button>Delete Cart</button> 
             <button>Place Your Order</button>
-            {cart.map((v,i)=>(
+            {ncart.map((v,i)=>(
                 <div className="row">
 
                     
@@ -36,7 +46,7 @@ export default function Cart({cart,setCart}){
                             {/* <img src={b[i] }alt="" className="card-img-top"/> */}
                             <div className="card-body"><h5 className="card-title">{v['name']}</h5>
                             <p className='card-text'> Price:{v['price']} Quantity {v['quantity']}</p></div>
-                            <button key={i} onClick={()=>remove(i)}>Remove Item</button>
+                            <button key={i}  className="btn btn-primary" onClick={()=>remove(i)}>Remove Item</button>
                         </div>
                     </div>
                 </div>
