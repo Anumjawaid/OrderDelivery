9b3bb17c924d9be302a21401c5293c3c52b3163b
Anumjawaid <anumjawaid99@gmail.com>
2021-04-16 09:01:30 +0500

    stackoverflow worked

--- src/components/Items/carts.jsx
+++ src/components/Items/carts.jsx
@@ -3,16 +3,16 @@ import './style.css';
 
 
 export default function Cart({cart,setCart}){
-    let [ncart,setNcart]=useState([])
+    let [ncart,setNcart]=useState()
     ncart=cart
 
     let [page,setPage]=useState()
     // console.log(cart[0]['name'][0],"from cart")
-    console.log("cart",cart,ncart)
-    ncart.map((v,i)=>(
-        console.log(v,"asdfgh")
+    // console.log("cart",cart,ncart)
+    // ncart.map((v,i)=>(
+    //     console.log(v,"asdfgh")
 
-       ))
+    //    ))
     
    
     const navigateto=(nextpage)=>{
@@ -20,6 +20,7 @@ export default function Cart({cart,setCart}){
 
     }
     const remove = (i)=>{
+        ncart=cart
         console.log("i am working",i)
         // delete cart[i]
         ncart.splice(i,1)
@@ -29,13 +30,21 @@ export default function Cart({cart,setCart}){
         // setNcart(Object.assign({}, ncart)) this can be used as well as setNcart({...ncart})
 // 
     }
+
+    const deletecart = ()=>{
+        setNcart([])
+        console.log("delete")
+        
+        console.log("ncart",ncart,"cart",cart)
+        setCart([])
+    }
     
 
     return(
         <div>
             <div className="container main"><h4>Items In Cart</h4>
             <h3>Your Cart Ittems</h3>
-            <button>Delete Cart</button> 
+            <button onClick={()=>setCart([])} className="btn btn-primary" >Delete Cart</button> 
             <button>Place Your Order</button>
             {ncart.map((v,i)=>(
                 <div className="row">
