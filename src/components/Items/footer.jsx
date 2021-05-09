c1ef90751fd55a80a975225602a5f44504955a7b
Anumjawaid <anumjawaid99@gmail.com>
2021-05-09 12:14:18 +0500

    footer element

---
+++ src/components/Items/footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer  (){
+    return(
+    <div class="footer">
+            <div className="foot">
+            <div className="foothead">
+            <h4>KHANA PAKANA</h4>
+            <p>We Deliver fresh,Delicious Authentic food in Pakistan.<br></br>
+              We Deliver fresh,Delicious Authentic food in Pakistan.<br></br>
+              We Deliver fresh,Delicious Authentic food in Pakistan</p>
+            </div>
+          
+          <div className="tags">
+          <h4>CONTACTS</h4>
+  
+            <p className='htag'>Home</p>
+            <p className='htag'>Price</p>
+            <p className='htag'>Contact</p>
+            </div>
+             </div>
+        <hr style={{color:'white'}}></hr>
+            <p>Copywright@khanapakana2021</p>
+            </div>
+    )
+          
+  
+  }
