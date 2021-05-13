59a8bec3be252c14b922dbd1cd74e58dc0cbcc88
Anumjawaid <anumjawaid99@gmail.com>
2021-05-13 23:49:57 +0500

    order added to firebase

--- src/components/Items/userinfo.jsx
+++ src/components/Items/userinfo.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { createFactory, useState } from 'react'
 import './style.css';
 import Pan from '../UI/panlogo.png'
 import cartim from '../UI/cart.png'
 import Deliver from '../UI/delivery.png'
 import Footer from './footer'
+import firebase from '../../config/firebase'
+
 
 
 export default function Info({ cart, setCart ,pr}) {
@@ -14,7 +16,21 @@ export default function Info({ cart, setCart ,pr}) {
     const[email,setEmail]=useState()
     const[phn,setPhone]=useState()
     const[address,setAddress]=useState()
+    const[orderd,setOrder]=useState()
 
+  
+    const addorder = ()=>{
+        const order={}
+        order.name=name
+        order.email=email
+        order.phn=phn
+        order.address=address
+        order.cart=cart
+        setOrder(order)
+        console.log(orderd,"order")
+        firebase.database().ref('/').child('orders').push(orderd)
+        
+    }
     return (
         <>
             <div className="aa">
@@ -43,14 +59,14 @@ export default function Info({ cart, setCart ,pr}) {
 
                         <div className="col-md-4 insideinfo">
                             <label>Your Name</label>
-                            <input type='text' placeholder="Enter Your Name" />
+                            <input type='text' placeholder="Enter Your Name" value={name} onChange={(e)=>setName(e.target.value)} />
                             <label>Your Phone</label>
-                            <input type='text' placeholder="Enter Your MobileNo" />
+                            <input type='text' placeholder="Enter Your MobileNo" value={phn} onChange={(e)=>setPhone(e.target.value)} />
                             <label>Your Email</label>
-                            <input type='text' placeholder="Enter Your Email" />
+                            <input type='text' placeholder="Enter Your Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
                             <label>Address</label>
-                            <textarea type='text' placeholder="Enter Your Address" />
-                            <button className='btn btn-primary co'>Confirm Order</button>
+                            <textarea type='text' placeholder="Enter Your Address" value={address} onChange={(e)=>setAddress(e.target.value)}/>
+                            <button className='btn btn-primary co' onClick={addorder}>Confirm Order</button>
                         </div>
                     </div>
                     <div className="col-md-6">
