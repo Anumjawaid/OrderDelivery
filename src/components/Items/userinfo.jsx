de3108ae08584ce3bd7c5f150dd62363dffd31c6
Anumjawaid <anumjawaid99@gmail.com>
2021-05-18 13:22:41 +0500

    order done 360

--- src/components/Items/userinfo.jsx
+++ src/components/Items/userinfo.jsx
@@ -8,115 +8,118 @@ import firebase from '../../config/firebase'
 
 
 
-export default function Info({ cart, setCart ,pr}) {
+export default function Info({ cart, setCart, pr }) {
 
-    console.log(cart,pr, "from userinfo")
+    console.log(cart, pr, "from userinfo")
 
-    const[name,setName]=useState()
-    const[email,setEmail]=useState()
-    const[phn,setPhone]=useState()
-    const[address,setAddress]=useState()
-    const[orderd,setOrder]=useState()
-    const[page,setPage]=useState('/order')
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+    const [phn, setPhone] = useState()
+    const [orderd, setOrder] = useState()
+    const [address, setAddress] = useState()
+    const [page, setPage] = useState('/order')
 
-  
-    const addorder = ()=>{
-        if(name=='' || email == '' || phn =='' || address == '' ){
-            // console.log("yahan if me aya hon")
+
+    const addorder = () => {
+        if (name == '' || email == '' || phn == '' || address == '') {
+            console.log("yahan if me aya hon")
             alert("Please Fill All Required Fields")
         }
-        else{
-        console.log("IN ORDER")
-        const order={}
-        order.name=name
-        order.email=email
-        order.phn=phn
-        order.address=address
-        order.cart=cart
-        setOrder(order)
-        console.log(order,"order")
-        try{
-        firebase.database().ref('/').child('orders').push(order)
-        setPage('/thankyou')
+        else {
 
-        }
-        catch{
-            setPage('/errortryagain')
+            console.log("IN ORDER")
+            const order = {}
+            order.name = name
+            order.email = email
+            order.phn = phn
+            order.address = address
+            order.cart = cart
+            setOrder(order)
+            console.log(order, "order")
+            try {
+                firebase.database().ref('/').child('orders').push(order)
+                setPage('/thankyou')
 
+            }
+            catch {
+                setPage('/errortryagain')
+
+            }
         }
-    }
     }
     return (
         <>
-        <div>
-        {
-            page==='/order' ? <div>
-                <div className="aa">
-                <nav className="navbar">
- 
-                    <a className="navbar-brand" href="#" >
-                        <img src={Pan} alt="" width="60" height="48" />KHANA PAKANA</a>
-                    <p className="cartimage">
-                        <img src={cartim} width='30px' />
-                        Items{cart.length ? <span style={{ color: 'brown', fontSize: '20px' }}>
-                             {cart.length}</span> : <span>*</span>}</p>
- 
-                </nav>
-            </div>
-            <div className="container usermain">
-                <div className="row">
-                    <div className="col-md-6">
-                        <div className="divimage">
-                            <img src={Deliver} id='imdeliver'/>
-                            <div className="descim">
-                            <h3>Please Fill in Your Details</h3>
-                            <h4>We Will Deliver You with Love And Care</h4>
-                            </div>
-                            </div>
-                            <div className="col-md-4 insideinfo">
-                            <label>Your Name</label>
-                            <input type='text' placeholder="Enter Your Name" value={name} onChange={(e)=>setName(e.target.value)} />
-                            <label>Your Phone</label>
-                            <input type='text' placeholder="Enter Your MobileNo" value={phn} onChange={(e)=>setPhone(e.target.value)} />
-                            <label>Your Email</label>
-                            <input type='text' placeholder="Enter Your Email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                            <label>Address</label>
-                            <textarea type='text' placeholder="Enter Your Address" value={address} onChange={(e)=>setAddress(e.target.value)}/>
-                            <button className='btn btn-primary co' onClick={addorder}>Confirm Order</button>
-                        </div>
-                    </div>
-                    <div className="col-md-6">
-                        <div className="cartcontainer">
-                            <h4>Your Order</h4>
-                            <ul>
-                            {cart.map((v,i)=>(
-                                <li key={i}>Item :{v['name'] } Price :{v['price']} </li>
- 
- 
-                            ))}
-                            <li>Sub Total :{pr}</li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-
-
-
-            </div>
-            
-            :page ==='/thankyou'?
             <div>
-                <h4>Thank you to shop with us</h4>
-            </div>
-             :
-             <div>
-            {alert("Eror Please TrY again")}
-            </div>
-        }
-            
-            <Footer />
+                {
+                    page === '/order' ? <div>
+                        <div className="aa">
+                            <nav className="navbar">
+
+                                <a className="navbar-brand" href="#" >
+                                    <img src={Pan} alt="" id='pan' width="60" height="48" />KHANA PAKANA</a>
+                                <p className="cartimage">
+                                    <img src={cartim} width='30px' />
+                        Items{cart.length ? <span style={{ color: 'brown', fontSize: '20px' }}>
+                                        {cart.length}</span> : <span>*</span>}</p>
+
+                            </nav>
+                        </div>
+                        <div className="container usermain">
+                            <div className="row">
+                                <div className="col-md-6">
+                                    <div className="divimage">
+                                        <img src={Deliver} id='imdeliver' />
+                                        <div className="descim">
+                                            <h3>Please Fill in Your Details</h3>
+                                            <h4>We Will Deliver You with Love And Care</h4>
+                                        </div>
+                                    </div>
+                                    <div className="col-md-4 insideinfo">
+                                        <label>Your Name</label>
+                                        <input type='text' placeholder="Enter Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                                        <label>Your Phone</label>
+                                        <input type='text' placeholder="Enter Your MobileNo" value={phn} onChange={(e) => setPhone(e.target.value)} />
+                                        <label>Your Email</label>
+                                        <input type='text' placeholder="Enter Your Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                                        <label>Address</label>
+                                        
+
+                                        <input  type='text' id='textarea' placeholder="Enter Your Address" value={address} onChange={(e) => setAddress(e.target.value)} />
+                                        <button className='btn btn-primary co' onClick={addorder}>Confirm Order</button>
+                                    </div>
+                                </div>
+                                <div className="col-md-6">
+                                    <div className="cartcontainer">
+                                        <h4>Your Order</h4>
+                                        <ul>
+                                            {cart.map((v, i) => (
+                                                <li key={i}>Item :{v['name']} Price :{v['price']} </li>
+
+
+                                            ))}
+                                            <li>Sub Total :{pr}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+                    </div>
+
+                        : page === '/thankyou' ?
+                            <div>
+                                <h4>Thank you to shop with us</h4>
+                            </div>
+                            :
+                            <div>
+                                {alert("Error Please Try Again")}
+                            </div>
+                }
+
+                <Footer />
             </div>
         </>
     )
